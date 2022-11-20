e6f5cedad18664d1b55a3b181ffd627d9f75c9db
if22b151 <115695058+if22b151@users.noreply.github.com>
2022-11-20 16:36:49 +0100

    Nav & Footer right included blackbox on the screen

--- FileUpload.php
+++ FileUpload.php
@@ -12,10 +12,12 @@ while($file=readdir($directoryHandler)){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Upload</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <title>FileUpload</title>
     <style>
         .black_background{
             background-image: url('img/background.jpg');
@@ -49,5 +51,8 @@ while($file=readdir($directoryHandler)){
     </div>
 
     <?php include 'php/footer.php' ?>
-    </body>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
 </html>

d5f85112480e5b81a4c6c810341d70b5f32c677b
Emile Durkheim <github@andretsch.at>
2022-11-21 17:19:08 +0100

    added non-existent variable error handling

--- php/scripts/login_logic.php
+++ php/scripts/login_logic.php
@@ -5,13 +5,16 @@ $valid_user = array(
     'name'=>'admin', 
     'email'=>'admin', 
     'pw'=>'admin'
-); 
+);
+
+require 'funcs.php';
+
+$email = get_default($_POST['email']);
+$pw = get_default($_POST['password']);
+
+$errors = array();
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $errors = array();
-    
-    $email = $_POST['email'];
-    $pw = $_POST['password'];
     
     if(empty($email) || empty($pw)){
         array_push($errors, "E-Mail und Passwort Feld müssen ausgefüllt sein!");
@@ -28,7 +31,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         array_push($errors, "E-Mail oder Passwort stimmen nicht überein!");
     }
     
-} elseif($_GET['logout']){
+} elseif(get_default($_GET['logout'])){
     session_destroy();
     header('location: /login.php');  // Gets stuck on logout page without this
 }

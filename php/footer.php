f00f42f0f07c55e18c66208168e0def4c9ec3f05
Emile Durkheim <github@andretsch.at>
2022-11-24 17:48:41 +0100

    Revert "footer now sizes down to icons at mobile screen size"
    This reverts commit cfa6c1570f14f370de5eea990b756149fbbf0ba3.

--- php/footer.php
+++ php/footer.php
@@ -1,31 +1,19 @@
 <footer class="container-fluid py-3 bg-dark">
-  <ul class="d-flex justify-content-center">
-    <li class="footer-item d-flex justify-content-center flex-grow-1 flex-sm-grow-0">
-      <a href="#">
-        <span class="d-none d-sm-inline">Twitter</span>
-        <i class="d-inline d-sm-none px-3 fa-brands fa-twitter fa-lg"></i>
-      </a>
+  <ul class="d-sm-flex justify-content-center">
+    <li class="footer-item d-flex justify-content-center d-sm-block">
+      <a href="#">Twitter</a>
     </li>
-    <li class="footer-item d-flex justify-content-center flex-grow-1 flex-sm-grow-0">
-      <a href="#">
-        <span class="d-none d-sm-inline">Facebook</span>
-        <i class="d-inline d-sm-none px-3 fa-brands fa-facebook fa-lg"></i>
-      </a>
+    <li class="footer-item  d-flex justify-content-center d-sm-block">
+      <a href="#">Facebook</a>
     </li>
-    <li class="footer-item d-none d-sm-block text-follow-us">
-      <span class="px-2">Social Media</span>
+    <li class="footer-item text-follow-us d-flex justify-content-center d-sm-block">
+      Social Media
     </li>
-    <li class="footer-item d-flex justify-content-center flex-grow-1 flex-sm-grow-0">
-      <a href="#">
-        <span class="d-none d-sm-inline">Instagram</span>
-        <i class="d-inline d-sm-none px-3 fa-brands fa-instagram fa-lg"></i>
-      </a>
+    <li class="footer-item d-flex justify-content-center d-sm-block">
+        <a href="#">Instagram</a>
     </li>
-    <li class="footer-item d-flex justify-content-center flex-grow-1 flex-sm-grow-0">
-      <a href="#">
-        <span class="d-none d-sm-inline">TikTok</span>
-        <i class="d-inline d-sm-none px-3 fa-brands fa-tiktok fa-lg"></i>
-      </a>
+    <li class="footer-item d-flex justify-content-center d-sm-block">
+        <a href="#">TikTok</a>
     </li>
   </ul>
 </footer>

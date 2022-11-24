ddee4ffa2da746c2171e6ece13dbc441c2469dbd
Emile Durkheim <github@andretsch.at>
2022-11-24 17:48:46 +0100

    Revert "Revert "footer now sizes down to icons at mobile screen size""
    This reverts commit f00f42f0f07c55e18c66208168e0def4c9ec3f05.

--- php/footer.php
+++ php/footer.php
@@ -1,19 +1,31 @@
 <footer class="container-fluid py-3 bg-dark">
-  <ul class="d-sm-flex justify-content-center">
-    <li class="footer-item d-flex justify-content-center d-sm-block">
-      <a href="#">Twitter</a>
+  <ul class="d-flex justify-content-center">
+    <li class="footer-item d-flex justify-content-center flex-grow-1 flex-sm-grow-0">
+      <a href="#">
+        <span class="d-none d-sm-inline">Twitter</span>
+        <i class="d-inline d-sm-none px-3 fa-brands fa-twitter fa-lg"></i>
+      </a>
     </li>
-    <li class="footer-item  d-flex justify-content-center d-sm-block">
-      <a href="#">Facebook</a>
+    <li class="footer-item d-flex justify-content-center flex-grow-1 flex-sm-grow-0">
+      <a href="#">
+        <span class="d-none d-sm-inline">Facebook</span>
+        <i class="d-inline d-sm-none px-3 fa-brands fa-facebook fa-lg"></i>
+      </a>
     </li>
-    <li class="footer-item text-follow-us d-flex justify-content-center d-sm-block">
-      Social Media
+    <li class="footer-item d-none d-sm-block text-follow-us">
+      <span class="px-2">Social Media</span>
     </li>
-    <li class="footer-item d-flex justify-content-center d-sm-block">
-        <a href="#">Instagram</a>
+    <li class="footer-item d-flex justify-content-center flex-grow-1 flex-sm-grow-0">
+      <a href="#">
+        <span class="d-none d-sm-inline">Instagram</span>
+        <i class="d-inline d-sm-none px-3 fa-brands fa-instagram fa-lg"></i>
+      </a>
     </li>
-    <li class="footer-item d-flex justify-content-center d-sm-block">
-        <a href="#">TikTok</a>
+    <li class="footer-item d-flex justify-content-center flex-grow-1 flex-sm-grow-0">
+      <a href="#">
+        <span class="d-none d-sm-inline">TikTok</span>
+        <i class="d-inline d-sm-none px-3 fa-brands fa-tiktok fa-lg"></i>
+      </a>
     </li>
   </ul>
 </footer>

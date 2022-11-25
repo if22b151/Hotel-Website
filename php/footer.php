819f32ea469118c376c86f22c9e3b1ae18072ec9
Emile Durkheim <github@andretsch.at>
2022-11-25 17:01:03 +0100

    simplified classes

--- php/footer.php
+++ php/footer.php
@@ -1,29 +1,29 @@
 <footer class="container-fluid py-3 bg-dark">
-  <ul class="d-flex justify-content-center">
-    <li class="footer-item d-flex justify-content-center flex-grow-1 flex-sm-grow-0">
+  <ul class="d-flex justify-content-around justify-content-sm-center">
+    <li class="footer-item">
       <a href="#">
-        <span class="d-none d-sm-inline">Twitter</span>
+        <span class="d-none d-sm-inline text-center">Twitter</span>
         <i class="d-inline d-sm-none px-3 fa-brands fa-twitter fa-lg"></i>
       </a>
     </li>
-    <li class="footer-item d-flex justify-content-center flex-grow-1 flex-sm-grow-0">
+    <li class="footer-item">
       <a href="#">
-        <span class="d-none d-sm-inline">Facebook</span>
+        <span class="d-none d-sm-inline text-center">Facebook</span>
         <i class="d-inline d-sm-none px-3 fa-brands fa-facebook fa-lg"></i>
       </a>
     </li>
     <li class="footer-item d-none d-sm-block text-follow-us">
       <span class="px-2">Social Media</span>
     </li>
-    <li class="footer-item d-flex justify-content-center flex-grow-1 flex-sm-grow-0">
+    <li class="footer-item">
       <a href="#">
-        <span class="d-none d-sm-inline">Instagram</span>
+        <span class="d-none d-sm-inline text-center">Instagram</span>
         <i class="d-inline d-sm-none px-3 fa-brands fa-instagram fa-lg"></i>
       </a>
     </li>
-    <li class="footer-item d-flex justify-content-center flex-grow-1 flex-sm-grow-0">
+    <li class="footer-item">
       <a href="#">
-        <span class="d-none d-sm-inline">TikTok</span>
+        <span class="d-none d-sm-inline text-center">TikTok</span>
         <i class="d-inline d-sm-none px-3 fa-brands fa-tiktok fa-lg"></i>
       </a>
     </li>

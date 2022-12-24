9a05b51b1c8a704f8e8a84f496a924c1aca26024
Emile Durkheim <github@andretsch.at>
2022-12-24 02:31:33 +0100

    added a comment

--- php/scripts/funcs.php
+++ php/scripts/funcs.php
@@ -2,14 +2,16 @@
 function is_empty_any(...$vars){
     foreach($vars as $var){
         if(empty($var)){
-            return TRUE;
+            return true;
         }
     }
 
-    return FALSE;
+    return false;
 }
 
 function get_default(&$var, $default=NULL){
+    // If variable doesn't exist yet, creates the variable and sets it to NULL.
+    // Prevents "unknown variable" errors when trying to set value=<?php echo $variable> in html forms
     if(isset($var)){
         return $var;
     } else {

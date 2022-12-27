2e190673b9bf725fa605db672dae3381e2aa0e74
Emile Durkheim <github@andretsch.at>
2022-12-27 01:27:46 +0100

    removed deprecated files

--- php/templates/article.php
+++
@@ -1,5 +0,0 @@
-<article>
-  <h2 class="mb-0">Title</h2>
-  <p class="text-muted"> <?php echo $article['author'] . ',' . $article['time']; ?> </p>
-  <p> <?php echo nl2br($article['text']); ?> </p>
-</article>

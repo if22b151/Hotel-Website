d0962254cfed7447f0f89e502b8fcc241b463f9d
Emile Durkheim <github@andretsch.at>
2022-12-27 01:24:28 +0100

    adjusted some css

--- news/upload.php
+++ news/upload.php
@@ -2,6 +2,7 @@
   session_start();
 
   require('../php/scripts/upload.php');
+  require_once('../php/scripts/funcs.php');
 ?>
 
 <!DOCTYPE html>
@@ -23,7 +24,7 @@
       <?php include '../php/navbar.php' ?>
 
       <div class="content-background flex-grow-1">
-      <div class="container site_content py-3">
+      <div class="container site_content px-3 py-3">
 
 
         <h1>News Beitrag erstellen</h1>
@@ -31,7 +32,7 @@
         <form enctype="multipart/form-data" method="post" action="">
           <div class="mb-3">
             <label class="form-label" for="title">Titel</label>
-            <input class="form-control" type="text" id="title" name="title" required>
+            <input class="form-control" type="text" id="title" name="title" value="<?=get_default($title)?>" required>
           </div>
           <div class="mb-3">
             <label class="form-label" for="image">Bild</label>
@@ -39,7 +40,7 @@
           </div>
           <div class="mb-3">
             <label class="form-label" for="content">Inhalt</label>
-            <textarea class="form-control" id="content" name="content" rows=10 required></textarea>
+            <textarea class="form-control" id="content" name="content" rows=10 value="<?=get_default($content)?>" required></textarea>
           </div>
 
           <input class="btn btn-secondary" type="submit" name="submit" value="Veröffentlichen">

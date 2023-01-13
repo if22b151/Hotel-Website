d7988e18eea5d6733f90da29836cbf1db2656d2a
Emile Durkheim <github@andretsch.at>
2023-01-13 19:11:51 +0100

    Redirects to index upon logout

--- php/scripts/logout.php
+++ php/scripts/logout.php
@@ -3,6 +3,6 @@ if($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['logout'])){
     session_destroy();
 
     // Reloads current page so logout changes can take effect
-    header('location: '. htmlspecialchars($_SERVER["PHP_SELF"]));
+    header('location: /index.php');
 }
 ?>
